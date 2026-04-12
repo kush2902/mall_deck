5a50a7c408006ca7e073d3946581afc081f47fa4
kush2902 <kushagra.singh2909@gmail.com>
2026-04-12 22:06:34 +0530

    Update SectionWrapper.tsx

--- components/SectionWrapper.tsx
+++ components/SectionWrapper.tsx
@@ -2,14 +2,18 @@
 
 import { motion } from "framer-motion";
 
-export default function SectionWrapper({ children }: any) {
+export default function SectionWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <motion.section
-      className="section"
       initial={{ opacity: 0, y: 80 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8 }}
       viewport={{ once: true }}
+      className="min-h-screen flex flex-col justify-center px-10 md:px-20"
     >
       {children}
     </motion.section>
