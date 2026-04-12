7c6a7985681a84d3b231dbf9909b99d79e81af1f
kush2902 <kushagra.singh2909@gmail.com>
2026-04-12 15:43:40 +0530

    Create CTA.tsx

---
+++ sections/CTA.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+export default function CTA() {
+  return (
+    <div className="text-center py-40">
+      <h2 className="text-5xl mb-8">Be Part of the Destination</h2>
+
+      <div className="flex justify-center gap-6">
+        <button className="px-8 py-4 bg-white text-black">
+          Lease Space
+        </button>
+        <button className="px-8 py-4 border border-white">
+          Partner With Us
+        </button>
+      </div>
+    </div>
+  );
+}
