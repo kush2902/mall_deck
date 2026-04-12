a4fe4afad53164559604ce5fe6583b38ef418874
kush2902 <kushagra.singh2909@gmail.com>
2026-04-12 15:44:15 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from "@/sections/Hero";
+import Why from "@/sections/Why";
+import Retail from "@/sections/Retail";
+import Entertainment from "@/sections/Entertainment";
+import CTA from "@/sections/CTA";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Why />
+      <Retail />
+      <Entertainment />
+      <CTA />
+    </main>
+  );
+}
