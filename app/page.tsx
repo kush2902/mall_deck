cfb049319fd0470c74365e80dcb93c86d12cfd1b
kush2902 <kushagra.singh2909@gmail.com>
2026-04-12 19:35:16 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Hero from "@/sections/Hero";
-import Why from "@/sections/Why";
-import Retail from "@/sections/Retail";
-import Entertainment from "@/sections/Entertainment";
-import CTA from "@/sections/CTA";
+import Hero from "../sections/Hero";
+import Why from "../sections/Why";
+import Retail from "../sections/Retail";
+import Entertainment from "../sections/Entertainment";
+import CTA from "../sections/CTA";
 
 export default function Home() {
   return (
