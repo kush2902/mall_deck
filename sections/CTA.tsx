4aa0a991f89803366eb913eb3b6ee62eaefe86f1
kush2902 <kushagra.singh2909@gmail.com>
2026-04-12 19:55:02 +0530

    Update CTA.tsx

--- sections/CTA.tsx
+++ sections/CTA.tsx
@@ -6,10 +6,10 @@ export default function CTA() {
       <h2 className="text-5xl mb-8">Be Part of the Destination</h2>
 
       <div className="flex justify-center gap-6">
-        <button className="px-8 py-4 bg-white text-black">
+        <button className="px-10 py-4 bg-white text-black rounded-full hover:scale-105 transition">
           Lease Space
         </button>
-        <button className="px-8 py-4 border border-white">
+        <button className="px-10 py-4 border border-white rounded-full hover:bg-white hover:text-black transition">
           Partner With Us
         </button>
       </div>
