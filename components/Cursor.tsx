e5b3695fd67683958d927758d62316c138dfbd87
kush2902 <kushagra.singh2909@gmail.com>
2026-04-13 12:15:29 +0530

    Create Cursor.tsx

---
+++ components/Cursor.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+
+export default function Cursor() {
+  const [pos, setPos] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const move = (e: MouseEvent) => {
+      setPos({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", move);
+    return () => window.removeEventListener("mousemove", move);
