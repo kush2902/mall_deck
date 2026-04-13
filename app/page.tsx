be5dc5f13024ba5330347de0d43e90a4066d6bbe
kush2902 <kushagra.singh2909@gmail.com>
2026-04-13 12:13:50 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,30 @@
+"use client";
+
+import { useState } from "react";
+import Loader from "../components/Loader";
+
 import Hero from "../sections/Hero";
 import Why from "../sections/Why";
 import Retail from "../sections/Retail";
 import Entertainment from "../sections/Entertainment";
-import CTA from "../sections/CTA";
 import Events from "../sections/Events";
 
-export default function Home() {
+export default function Page() {
+  const [loaded, setLoaded] = useState(false);
+
   return (
-    <main>
-      <Hero />
-      <Why />
-      <Retail />
-      <Entertainment />
-      <Events />
-      <CTA />
-    </main>
+    <>
+      {!loaded && <Loader onFinish={() => setLoaded(true)} />}
+
+      {loaded && (
+        <>
+          <Hero />
+          <Why />
+          <Retail />
+          <Entertainment />
+          <Events />
+        </>
+      )}
+    </>
   );
 }
