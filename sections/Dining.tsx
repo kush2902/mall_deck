a72541aeeda550cbfa103b31959f18f53720602d
kush2902 <kushagra.singh2909@gmail.com>
2026-04-19 17:33:44 +0530

    Update Dining.tsx

--- sections/Dining.tsx
+++ sections/Dining.tsx
@@ -9,7 +9,7 @@ type Props = {
   setActive: (key: string) => void;
 };
 
-export default function Dining() {
+export default function Dining({ setActive }: Props) {
   return (
     <SlideWrapper>
       <BackToHub setActive={setActive} />
