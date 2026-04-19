214ac5d982a9412a8e29999226ca3be3d7237f5e
kush2902 <kushagra.singh2909@gmail.com>
2026-04-19 12:57:46 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,11 @@ import Retail from "../sections/Retail";
 import Entertainment from "../sections/Entertainment";
 import Opportunities from "../sections/Opportunities";
 
+import RetailLeasing from "../sections/RetailLeasing";
+import BrandPartnership from "../sections/BrandPartnership";
+import EventBooking from "../sections/EventBooking";
+import Venue from "../sections/Venue";
+
 export default function Home() {
   return (
     <Deck>
@@ -14,6 +19,10 @@ export default function Home() {
       <Retail />
       <Entertainment />
       <Opportunities />
+      <RetailLeasing />
+      <BrandPartnership />
+      <EventBooking />
+      <Venue />
     </Deck>
   );
 }
