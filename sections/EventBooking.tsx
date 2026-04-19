2635c7ad7b8b57251f9609f982a6785d99008bbf
kush2902 <kushagra.singh2909@gmail.com>
2026-04-19 13:15:31 +0530

    Update EventBooking.tsx

--- sections/EventBooking.tsx
+++ sections/EventBooking.tsx
@@ -6,7 +6,7 @@ export default function EventBooking() {
   return (
     <SlideWrapper>
       <video autoPlay muted loop className="absolute w-full h-full object-cover">
-        <source src="YOUR_EVENTS_VIDEO_URL" />
+        <source src="/video/events.mp4" />
       </video>
 
       <div className="absolute inset-0 bg-black/60" />
