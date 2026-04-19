62a1f8663d1611594a8728b2d24b59419eefb6b8
kush2902 <kushagra.singh2909@gmail.com>
2026-04-19 13:43:32 +0530

    Update video source path in Venue component

--- sections/Venue.tsx
+++ sections/Venue.tsx
@@ -7,7 +7,7 @@ export default function Venue() {
   return (
     <SlideWrapper>
       <video autoPlay muted loop className="absolute w-full h-full object-cover">
-        <source src="/video/venue.mp4" />
+        <source src="/videos/venue.mp4" />
       </video>
 
       <div className="absolute inset-0 bg-black/60" />
