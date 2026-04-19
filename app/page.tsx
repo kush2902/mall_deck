03b82046837a0534a7f776ebc18f0fb5924731b2
kush2902 <kushagra.singh2909@gmail.com>
2026-04-19 15:27:08 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,7 @@
+"use client";
+
 import Deck from "../components/Deck";
 
-import Hero from "../sections/Hero";
-import Why from "../sections/Why";
-import Retail from "../sections/Retail";
-import Entertainment from "../sections/Entertainment";
-import Dining from "../sections/Dining";
-import NavHub from "../sections/NavHub";
-import RetailLeasing from "../sections/RetailLeasing";
-import BrandPartnership from "../sections/BrandPartnership";
-import EventBooking from "../sections/EventBooking";
-import Venue from "../sections/Venue";
-
 export default function Home() {
-  return (
-    <Deck>
-      <Hero />
-      <Why />
-      <NavHub /> 
-      <Retail />
-      <Dining /> 
-      <Entertainment />
-      <RetailLeasing />
-      <BrandPartnership />
-      <EventBooking />
-      <Venue />
-    </Deck>
-  );
+  return <Deck />;
 }
